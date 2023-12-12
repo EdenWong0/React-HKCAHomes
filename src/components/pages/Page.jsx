3cfab5b406ab4ab0276c5daea59006ec072430bb
EdenWong0 <wedenpakhin@gmail.com>
2023-12-11 23:34:57 -0500

    first commit

---
+++ src/components/pages/Page.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Header from '../basic/header/Header'
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
+
+const Page = () => {
+  return (
+    <>
+      <Router>
+        <Header />
+        <Switch>
+          
+        </Switch>
+      </Router>
+    </>
+  )
+}
+
+export default Page
